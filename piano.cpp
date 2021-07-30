be227d66b7e2b9040dc06f0fd35e520764b478c4
lzanelzaz <lzanelzaz@gmail.com>
2021-07-30 20:04:18 +0700

    Reduce buttons quantity

--- piano.cpp
+++ piano.cpp
@@ -2,46 +2,45 @@
 
 Piano::Piano(int height, int width)
 {
-    QString whiteKeys[14] = {"C", "D", "E", "F", "G", "A", "B", "C", "D", "E", "F", "G", "A", "B"};
-    QString blackKeys[10] = {"C#", "D#", "F#", "G#", "A#", "C#", "D#", "F#", "G#", "A#"};
+    QString whiteKeys[7] = {"C", "D", "E", "F", "G", "A", "B"};
+    QString blackKeys[5] = {"C#", "D#", "F#", "G#", "A#"};
     //Layout setup
     QGridLayout* pvbxLayout = new QGridLayout;
     QHBoxLayout* phbxLayoutWhite = new QHBoxLayout;
     phbxLayoutWhite->setContentsMargins(28, 30, 28, 20);
     phbxLayoutWhite->setSpacing(0);
-    for (int i = 0; i < 14; ++i) {
+    for (int i = 0; i < 7; ++i) {
         phbxLayoutWhite->addWidget(createWhiteButton(whiteKeys[i], height, width));
     }
 
     QHBoxLayout* phbxLayoutBlack = new QHBoxLayout;
     phbxLayoutBlack->setSpacing(0);
     phbxLayoutBlack->setContentsMargins(28, 30, 28, 20 + height/2 - 25);
-    QSpacerItem* transparent = new QSpacerItem(width/24 - 7, height);
-    for (int i = 0; i < 10; ++i) {
-        phbxLayoutBlack->addSpacerItem(transparent);
-        phbxLayoutBlack->addWidget(createBlackButton(blackKeys[i], height, width));
-        i++;
-        phbxLayoutBlack->addSpacerItem(transparent);
-        phbxLayoutBlack->addWidget(createBlackButton(blackKeys[i], height, width));
-        phbxLayoutBlack->addSpacerItem(transparent);
-        phbxLayoutBlack->addSpacerItem(transparent);
-        i++;
-        phbxLayoutBlack->addWidget(createBlackButton(blackKeys[i], height, width));
-        phbxLayoutBlack->addSpacerItem(transparent);
-        i++;
-        phbxLayoutBlack->addWidget(createBlackButton(blackKeys[i], height, width));
-        phbxLayoutBlack->addSpacerItem(transparent);
-        i++;
-        phbxLayoutBlack->addWidget(createBlackButton(blackKeys[i], height, width));
-        phbxLayoutBlack->addSpacerItem(transparent);
-    }
+    QSpacerItem* transparent = new QSpacerItem(width/12 - 14, height);
+
+    phbxLayoutBlack->addSpacerItem(transparent);
+    phbxLayoutBlack->addWidget(createBlackButton(blackKeys[0], height, width));
+
+    phbxLayoutBlack->addSpacerItem(transparent);
+    phbxLayoutBlack->addWidget(createBlackButton(blackKeys[1], height, width));
+    phbxLayoutBlack->addSpacerItem(transparent);
+    phbxLayoutBlack->addSpacerItem(transparent);
+
+    phbxLayoutBlack->addWidget(createBlackButton(blackKeys[2], height, width));
+    phbxLayoutBlack->addSpacerItem(transparent);
+
+    phbxLayoutBlack->addWidget(createBlackButton(blackKeys[3], height, width));
+    phbxLayoutBlack->addSpacerItem(transparent);
+
+    phbxLayoutBlack->addWidget(createBlackButton(blackKeys[4], height, width));
+    phbxLayoutBlack->addSpacerItem(transparent);
+
     pvbxLayout->addLayout(phbxLayoutWhite, 0, 0);
     pvbxLayout->addLayout(phbxLayoutBlack, 0, 0);
     setLayout(pvbxLayout);
 }
 
-template <typename T>
-void font_size (T pointer, int size)
+void font_size(QPushButton* pointer, int size)
 {
     QFont font = pointer->font();
     font.setPointSize(size);
@@ -51,7 +50,7 @@ void font_size (T pointer, int size)
 QPushButton* Piano::createWhiteButton(const QString& str, int height, int width){
     QPushButton* pButton = new QPushButton(str);
     pButton->setStyleSheet("color: black; background-color: white");
-    pButton->setMaximumSize(width/14 - 12, height - 50);
+    pButton->setMaximumSize(width/7 - 24, height - 50);
     font_size(pButton, 40);
     QSoundEffect* pKey = new QSoundEffect;
     pKey->setSource(QUrl::fromLocalFile(QString(":/sounds/%1.wav").arg(str)));
@@ -62,7 +61,7 @@ QPushButton* Piano::createWhiteButton(const QString& str, int height, int width)
 QPushButton* Piano::createBlackButton(const QString& str, int height, int width){
     QPushButton* pButton = new QPushButton(str);
     pButton->setStyleSheet("color: white; background-color: black");
-    pButton->setMaximumSize(width/24 - 7, height/2 - 25);
+    pButton->setMaximumSize(width/12 - 14, height/2 - 25);
     font_size(pButton, 40);
     QSoundEffect* pKey = new QSoundEffect;
     pKey->setSource(QUrl::fromLocalFile(QString(":/sounds/%1.wav").arg(str)));
